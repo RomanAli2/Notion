581f348f0cbee5efc873e346512f5b7412cfa986
RomanAli2 <romanali87866677@gmail.com>
2026-02-12 05:36:56 -0800

    ''first

--- src/components/notionfree.vue
+++ src/components/notionfree.vue
@@ -54,7 +54,7 @@ import { router } from '../router';
 import { ref } from 'vue';
 const UserEmail=ref("");
 function DashboardGo(){
-  router.push("/DashboardPage/sidebarHomePage'age")
+  router.push("/DashboardPage/sidebarHomePage'")
 }
 function LoginBtn(){
   if(UserEmail.value==="notion009@gmail.com"){
